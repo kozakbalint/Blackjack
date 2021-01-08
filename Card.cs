a9d27281de866011e179efa6bcd634f55895dd38
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2021-01-08 19:25:18 +0100

    Card and DeckOfCards class created.

---
+++ Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Card
+    {
+        public enum SUIT
+        {
+            CLUBS,
+            DIAMONDS,
+            HEARTS,
+            SPADES
+        }
+
+        public enum VALUE
+        {
+            TWO,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            KING,
+            QUEEN,
+            JACK,
+            ACE
+        }
+
+        public SUIT MySuit { get; set; }
+        public VALUE MyValue { get; set; }
+    }
+}
---
+++ DeckOfCards.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class DeckOfCards : Card
+    {
+        const int NUMBER_OF_CARDS = 52;
+        private Card[] deck;
+
+        public DeckOfCards()
+        {
+            deck = new Card[NUMBER_OF_CARDS];
+        }
+
+        public Card[] GetDeck => deck;
+
+        public void fillDeck()
+        {
+            int i = 0;
+            foreach (SUIT suit in Enum.GetValues(typeof(SUIT)))
+            {
+                foreach (VALUE value in Enum.GetValues(typeof(VALUE)))
+                {
+                    deck[i] = new Card { MySuit = suit, MyValue = value };
+                    i++;
+                }
+            }
+            shuffleCards();
+        }
+
+        public void shuffleCards()
+        {
+            Random r = new Random();
+            Card tmp;
+            for (int i = 0; i < 150; i++)
+            {
+                for (int j = 0; j < NUMBER_OF_CARDS; j++)
+                {
+                    int secondCard = r.Next(13);
+                    tmp = deck[j];
+                    deck[j] = deck[secondCard];
+                    deck[secondCard] = tmp;
+                }
+            }
+        }
+    }
+}
