caa27b4d2bb94c7cb8394c1fb545cc62424cd70d
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2021-01-10 16:46:10 +0100

    Result class added.

---
+++ Result.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Result
+    {
+        private Card[] playerHand; 
+        private Card[] dealerHand;
+        private string outcome;
+
+        public Result(Card[] playerHand, Card[] dealerHand, string outcome)
+        {
+            this.playerHand = playerHand;
+            this.dealerHand = dealerHand;
+            this.outcome = outcome;
+        }
+    }
+}
