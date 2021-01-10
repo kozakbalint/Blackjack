69109a52356cb2df9be3ac8b6bb57d393dab5188
Bálint Kozák <43552747+kozakbalint@users.noreply.github.com>
2021-01-10 13:23:29 +0100

    Player class added.

---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Player
+    {
+        private string name;
+        private int balance;
+
+        public Player(int balance)
+        {
+            this.balance = balance;
+        }
+
+        public string GetName => name;
+        public int GetBalance => balance;
+        public string SetName { set => name = value; }
+        public string SetBalance { set => balance = Int32.Parse(value); }
+    }
+}
